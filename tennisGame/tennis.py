ccd1904df3dccf959a2b5d7806ffd89e55f5a487
Minato007 <yellow_sama@mail.ru>
2018-12-10 12:03:06 +0600

    v2.0.0

--- tennisGame/tennis.py
+++ tennisGame/tennis.py
@@ -32,7 +32,10 @@ size = (540, 500)
 
 player1 = Player(5, 100, 20, 100)
 player2 = Player(size[0]-5-20, 100, 20, 100)
+
+balls = []
 ball = Ball(size[0]/2-20, size[1]/2-20, 40)
+balls.append(ball)
 
 pygame.init()
 pygame.font.init() # you have to call this at the start,
@@ -46,9 +49,28 @@ done = False
 
 c = 0
 
+clock = pygame.time.Clock()
+counter = 5
+text = '5'
+pygame.time.set_timer(pygame.USEREVENT, 1000)
+font = pygame.font.SysFont('Consolas', 30)
+
 while not done:
 
     for event in pygame.event.get():
+
+        if event.type == pygame.USEREVENT:
+            counter -= 1
+            if counter > 0:
+                text = str(counter)
+            else:
+                text = 'new ball'
+                ball = Ball(size[0]/2-20, size[1]/2-20, 40)
+                ball.move = True
+                balls.append(ball)
+                counter = 5
+                text = '5'
+
         if event.type == pygame.QUIT:
             done = True
 
@@ -88,39 +110,45 @@ while not done:
         if player2.y > size[1] - 1 - player2.height:
             player2.y = size[1] - 1 - player2.height
 
-        if ball.move:
-            ball.x += ball.dx
-            ball.y += ball.dy
+        for ball in balls:
+            if ball.move:
+                ball.x += ball.dx
+                ball.y += ball.dy
 
         # Bounce
+        for ball in balls:
+            if ball.x > size[0] - ball.size - player2.width:
+                if (ball.y + ball.size/2 > player2.y) and (ball.y + ball.size/2 < player2.y + player2.height):
+                    ball.dx = -abs(ball.dx)
+                    ball.dx = ball.dx * 1.1
+                    ball.dy = ball.dy * 1.1
+                else:
+                    # ball = Ball(size[0] / 2 - 20, size[1] / 2 - 20, 40)
+                    # ball.move = True
+                    balls.remove(ball)
+                    player1.score += 1
+                    c = 0
 
-        if ball.x > size[0] - ball.size - player2.width:
-            if (ball.y + ball.size/2 > player2.y) and (ball.y + ball.size/2 < player2.y + player2.height):
-                ball.dx = -abs(ball.dx)
-                ball.dx = ball.dx * 1.1
-                ball.dy = ball.dy * 1.1
-            else:
-                ball = Ball(size[0] / 2 - 20, size[1] / 2 - 20, 40)
-                ball.move = True
-                player1.score += 1
-                c = 0
+        for ball in balls:
+            if ball.y > size[1] - ball.size:
+                ball.dy = -abs(ball.dy)
 
-        if ball.y > size[1] - ball.size:
-            ball.dy = -abs(ball.dy)
+        for ball in balls:
+            if ball.x < player1.x + player1.width:
+                if (ball.y + ball.size/2 > player1.y) and (ball.y + ball.size/2 < player1.y + player1.height):
+                    ball.dx = abs(ball.dx)
+                    ball.dx = ball.dx * 1.1
+                    ball.dy = ball.dy * 1.1
+                else:
+                    # ball = Ball(size[0] / 2 - 20, size[1] / 2 - 20, 40)
+                    # ball.move = True
+                    balls.remove(ball)
+                    player2.score += 1
+                    c = 0
 
-        if ball.x < player1.x + player1.width:
-            if (ball.y + ball.size/2 > player1.y) and (ball.y + ball.size/2 < player1.y + player1.height):
-                ball.dx = abs(ball.dx)
-                ball.dx = ball.dx * 1.1
-                ball.dy = ball.dy * 1.1
-            else:
-                ball = Ball(size[0] / 2 - 20, size[1] / 2 - 20, 40)
-                ball.move = True
-                player2.score += 1
-                c = 0
-
-        if ball.y < 1:
-            ball.dy = abs(ball.dy)
+        for ball in balls:
+            if ball.y < 1:
+                ball.dy = abs(ball.dy)
 
         c += 10
         if c > 255:
@@ -138,13 +166,16 @@ while not done:
         player2.x, player2.y, player2.width, player2.height
     ], 0)
 
-    screen.blit(ball.img, (ball.x, ball.y))
+    for ball in balls:
+        screen.blit(ball.img, (ball.x, ball.y))
 
     text_surface = myfont.render(str(player1.score),False,(20,20,250))
     screen.blit(text_surface, (size[0]/3, 10))
 
     text_surface = myfont.render(str(player2.score),False,(20,20,250))
     screen.blit(text_surface, (size[0]*2/3, 10))
+
+    screen.blit(font.render(text, True, (0, 0, 0)), (250, 10)) #вывод каунтера на экран
 
     if (player1.score == 3) or (player2.score == 3):
         state = 'gameOver'
